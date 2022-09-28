21797dc724a25c6c9b1e2d2be74c7e37bc92e6ac
Camilla Devasini <camilladevasini@gmail.com>
2022-09-28 23:33:44 +0200

    add exercise jsx-01

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+//OPPURE const title = <h1>Hello World</h1> e poi dopo all'interno di return --> {title}
+
 function App() {
+  const title = "Hello World";
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{title}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
