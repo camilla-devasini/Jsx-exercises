4cd8f24ed915befb411cc6e49ef01a6f7e954dd0
Camilla Devasini <camilladevasini@gmail.com>
2022-10-02 23:28:42 +0200

    fix exercise jsx-05

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import sum from './components/sumTitle.js';
+
 
 // Write a function that receives two parameters, a and b
 // and returns the sum of them within an h2 tag.
-
+function sum(a,b) {
+  return <h2>The sum is {a + b}</h2>
+}
 
 function App() {
   return (
--- src/components/sumTitle.js
+++
@@ -1,5 +0,0 @@
-function sum(a,b) {
-    return <h2>The sum is {a + b}</h2>
-}
-
-export default sum;
